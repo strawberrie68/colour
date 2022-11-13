e71e6b0bcf4804deb6e22e0af80d94290aa09aba
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2022-11-13 08:14:39 -0800

    format

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const resetButton = document.querySelector('#reset');
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
