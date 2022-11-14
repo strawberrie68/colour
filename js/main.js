67bb28a6e85f2467f807d1f37e2e9e728ae9c105
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2022-11-14 15:29:40 -0800

    colour

--- js/main.js
+++ js/main.js
@@ -8,9 +8,29 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 let wrongRandomColor = Math.floor(Math.random()*16777215).toString(16);
+let hexColour = "123456789abcdefghijklmnopqrstuvwxyz".split('')
 
-let rightRandomColor = wrongRandomColor.slice(-1) + Math.random()
+let rightRandomColor = 
+ hexColour.indexOf(wrongRandomColor.charAt(4))
+if(indexColour ===  34){
+    indexColour-=1
+    rightRandomColor.replace(wrongRandomColor[4],hexColour[indexColour] )
+}
+else{
+    indexColour += 1
+    rightRandomColor.replace(wrongRandomColor[4],hexColour[indexColour] )
+}
 
+console.log(wrongRandomColor)
+console.log(rightRandomColor)
+
+
+//need to idenify the last charater
+
+//and match to the array then add one
+//array = ["123456789abcdefghijklmnopqrstuvwxyz"]
+//if at the end of the array turn -1
+//replace with new letter or number
 
 
 
