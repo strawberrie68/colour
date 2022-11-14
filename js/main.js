a96cb290a2dedc2aacd885e864b254a0c9e2fcba
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2022-11-13 23:46:31 -0800

    color

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@ window.addEventListener('DOMContentLoaded', () => {
     const tiles = Array.from(document.querySelectorAll('.tile'));
     const resetButton = document.querySelector('#reset');
 
-    
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let isGameActive = true;
 
 
 
+let wrongRandomColor = Math.floor(Math.random()*16777215).toString(16);
 
+let rightRandomColor = wrongRandomColor.slice(-1) + Math.random()
 
 
 
